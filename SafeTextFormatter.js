1fb2e3124c7381544830f285b0e20cb866739bef
usy <contact.usyless@gmail.com>
2024-08-06 02:56:39 +0100

    add header support

--- SafeTextFormatter.js
+++ SafeTextFormatter.js
@@ -1,68 +1,91 @@
 (() => {
-    const big_code_block = /```.*?\n```/gs;
-    const small_code_block = /`.*?`/g;
-    const bold_regex = /\*\*.*?\*\*/gs;
-    // do headers, links
+    const big_code_block = {
+        regex: /```.*?\n```/gs,
+        special: (t) => {
+            const holding_div = document.createElement('div'), pre = document.createElement('pre'), code = document.createElement('code'), info_div = document.createElement('div'), language_div = document.createElement('div'), copy_button = document.createElement('button');
+            const lines = t.split('\n');
+            let language = lines[0].substring(3).trim();
+            if (language.length === 0) language = 'No Language Specified';
+            holding_div.classList.add('codeBlockLanguageOuter');
+            pre.appendChild(code);
+            info_div.classList.add('codeBlockLanguage');
+            holding_div.append(info_div, pre);
+            info_div.append(language_div, copy_button);
+            language_div.textContent = language;
+            language_div.classList.add('languageDiv');
+            copy_button.textContent = 'Copy';
+            copy_button.addEventListener('click', () => navigator.clipboard.writeText(code.textContent));
+            copy_button.classList.add('standardButton');
+            lines.shift();
+            lines.pop();
+            code.textContent = lines.join('\n');
+            return holding_div;
+        }
+    }
+    const small_code_block = {
+        regex: /`.*?`/g,
+        cutoff: (t) => (t.substring(1, t.length - 1)),
+        tag: 'code'
+    }
+    const bold_regex = {
+        regex: /\*\*.*?\*\*/gs,
+        cutoff: (t) => (t.substring(2, t.length - 2)),
+        tag: 'b'
+    }
+    const italic_regex = {
+        regex: /\*.*?\*/gs,
+        cutoff: (t) => (t.substring(1, t.length - 1)),
+        tag: 'i'
+    }
+    const h3_regex = {
+        regex: /###.*/g,
+        cutoff: (t) => (t.substring(3)),
+        tag: 'h3'
+    }
+    const h2_regex = {
+        regex: /##.*/g,
+        cutoff: (t) => (t.substring(2)),
+        tag: 'h2'
+    }
+    const h1_regex = {
+        regex: /#.*/g,
+        cutoff: (t) => (t.substring(1)),
+        tag: 'h1'
+    }
+    // TODO: links
 
     // Return the text as an array of nodes to insert into an element
     function getFormatted(text) {
         const output = [];
 
-        const big_code_matches = text.match(big_code_block) || [];
-        let first_iterator = 0;
-        for (let one of text.split(big_code_block)) {
-            while (one.substring(0, 1) === '\n') one = one.substring(2); // Remove initial newlines
-
-            const small_code_matches = one.match(small_code_block) || [];
-            let second_iterator = 0;
-            for (const two of one.split(small_code_block)) {
-
-                const bold_matches = two.match(bold_regex) || [];
-                let third_iterator = 0;
-                for (const three of two.split(bold_regex)) {
-                    output.push(document.createTextNode(three));
-
-                    // bold text
-                    if (bold_matches[third_iterator]) {
-                        const bold = document.createElement('b');
-                        bold.textContent = bold_matches[third_iterator].substring(2, bold_matches[third_iterator].length - 2);
-                        output.push(bold);
-                    }
-                    ++third_iterator;
-                }
-
-                // inline code blocks
-                if (small_code_matches[second_iterator]) {
-                    const code = document.createElement('code');
-                    code.textContent = small_code_matches[second_iterator].substring(1, small_code_matches[second_iterator].length - 1);
-                    output.push(code);
-                }
-                ++second_iterator;
-            }
-
-            // multi line code blocks
-            if (big_code_matches[first_iterator]) {
-                const holding_div = document.createElement('div'), pre = document.createElement('pre'), code = document.createElement('code'), info_div = document.createElement('div'), language_div = document.createElement('div'), copy_button = document.createElement('button');
-                const lines = big_code_matches[first_iterator].split('\n');
-                let language = lines[0].substring(3).trim();
-                if (language.length === 0) language = 'No Language Specified';
-                holding_div.classList.add('codeBlockLanguageOuter');
-                pre.appendChild(code);
-                info_div.classList.add('codeBlockLanguage');
-                holding_div.append(info_div, pre);
-                info_div.append(language_div, copy_button);
-                language_div.textContent = language;
-                language_div.classList.add('languageDiv');
-                copy_button.textContent = 'Copy';
-                copy_button.addEventListener('click', () => navigator.clipboard.writeText(code.textContent));
-                copy_button.classList.add('standardButton');
-                lines.shift();
-                lines.pop();
-                output.push(holding_div);
-                code.textContent = lines.join('\n');
-            }
-            ++first_iterator;
+        function pushBasicElement(tag, text, cutoff_func) {
+            const elem = document.createElement(tag);
+            elem.textContent = cutoff_func(text);
+            output.push(elem);
         }
+
+        function regexIterator(text, replacementInfo, innerIterator) {
+            const matches = text.match(replacementInfo.regex) || [], special = replacementInfo.special;
+            let iterator = 0;
+            for (const non_match of text.split(replacementInfo.regex)) {
+                innerIterator(non_match);
+                if (matches[iterator]) {
+                    if (!special) pushBasicElement(replacementInfo.tag, matches[iterator], replacementInfo.cutoff);
+                    else output.push(special(matches[iterator]))
+                }
+                ++iterator;
+            }
+        }
+
+        regexIterator(text, big_code_block,
+            (t) => regexIterator(t, h3_regex,
+                (t) => regexIterator(t, h2_regex,
+                    (t) => regexIterator(t, h1_regex,
+                        (t) => regexIterator(t, small_code_block,
+                            (t) => regexIterator(t, bold_regex,
+                                (t) => regexIterator(t, italic_regex,
+                                    (t) => output.push(document.createTextNode(t)))))))));
+
         return output;
     }
 
